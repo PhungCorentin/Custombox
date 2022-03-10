0bf93477429669f7ee45dd2457b1b2f2b4f1e7c2
Ariirau <ariirau.fucks7@etu.univ-lorraine.fr>
2022-03-10 10:49:37 +0100

    Ajout modele

---
+++ src/modele/Boite.php
@@ -0,0 +1,8 @@
+<?php
+
+class Boite extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'boite';
+    protected $primaryKey = 'id';
+    public $timestamps = 'false';
+}
---
+++ src/modele/Categorie.php
@@ -0,0 +1,8 @@
+<?php
+
+class Categorie extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'categorie';
+    protected $primaryKey = 'id';
+    public $timestamps = 'false';
+}
